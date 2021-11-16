89d614f89a5ee53ccaba1678452f016264e13f42
Dirico19 <diego090819@gmail.com>
2021-11-15 23:05:29 -0500

    prueba 5

--- js/encabezado.js
+++ js/encabezado.js
@@ -1,6 +1,6 @@
 addEventListener("load",()=>{
     const header = document.querySelector("header");
-    fetch("./pages/encabezado.html")
+    fetch("./encabezado.html")
     .then(res => res.text())
     .then(data => {
         header.innerHTML = data;
